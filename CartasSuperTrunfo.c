e2dc4fd106eed86ed4ae19a1282abd6d47263f45
erickrtomaz <erickrtomaz@gmail.com>
2025-10-29 00:11:19 +0000

    Implementacao do if else

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -67,38 +67,35 @@ int main() {
     printf("\n== CARTA 02 ==\n");
     printf("Estado: %s\nCidade: %s\nCódigo: %s\nPopulação: %lu\nÁrea: %.2f\nPIB: %.2f\nPontos turísticos: %lu\n",
            Estado2, Nomedacidade2, Codigodacarta2, Populacao2, Area2, PIB2, Pontos2);
-    printf("Densidade populacional: %.2f\nPIB per capita: %.2f\nSuper poder: %.2f\n",
+    printf("Densidade populacional: %.2f\nPIB per capita: %.2f\nSuper poder: %.2f\n\n",
            Densidade2, PIBpercapta2, Superpoder2);
 
-    // Comparações apenas com operadores (1 = venceu, 0 = perdeu)
-    int comp_pop = Populacao1 > Populacao2;
-    int comp_area = Area1 > Area2;
-    int comp_pib = PIB1 > PIB2;
-    int comp_pontos = Pontos1 > Pontos2;
-    int comp_pibcap = PIBpercapta1 > PIBpercapta2;
-    int comp_dens = Densidade1 < Densidade2; // menor vence
-    int comp_super = Superpoder1 > Superpoder2;
 
-    // Resultados impressos como valores 0 e 1
-    printf("\n=== RESULTADOS DAS COMPARAÇÕES ===\n");
-    printf("População: %d\n", comp_pop);
-    printf("Área: %d\n", comp_area);
-    printf("PIB: %d\n", comp_pib);
-    printf("Pontos turísticos: %d\n", comp_pontos);
-    printf("PIB per capita: %d\n", comp_pibcap);
-    printf("Densidade populacional: %d\n", comp_dens);
-    printf("Super poder: %d\n", comp_super);
+           //Comparação com if e else
 
-    // Soma de pontos (1 ponto para cada atributo vencido)
-    int pontos_carta1 = comp_pop + comp_area + comp_pib + comp_pontos + comp_pibcap + comp_dens + comp_super;
-    int pontos_carta2 = 7 - pontos_carta1;
+           if (Populacao1 > Populacao2)
+           {
+              printf("Carta 1 (%s) Venceu - Populacao = (%lu) \n" ,Estado1,Populacao1);
+           }else
+           {
+              printf("Carta 2 (%s) Venceu - Populacao = (%lu) \n" ,Estado2,Populacao2);
+           } 
 
-    printf("\nTotal de vitórias da carta 1: %d\n", pontos_carta1);
-    printf("Total de vitórias da carta 2: %d\n", pontos_carta2);
+           if (Densidade1 < Densidade2)
+           {
+              printf("Carta 1 (%s) Venceu - Densidade = (%f)\n" ,Estado1,Densidade1);
+           }else
+           {
+              printf("Carta 2 (%s) Venceu - Densidade = (%f)\n",Estado2,Densidade2);
+           }
+           
+           
+           
 
-    // Diferença mostra vantagem
-    int vantagem = pontos_carta1 - pontos_carta2;
-    printf("Diferença de vitórias (Carta1 - Carta2): %d\n", vantagem);
+
+
+
+
 
     return 0;
 }
